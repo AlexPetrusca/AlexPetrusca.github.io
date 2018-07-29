0af8b2d20f60c8f817a84be94aaf7bce8f2b1c05
AlexPetrusca <alexandru.petrusca@yahoo.com>
2018-07-28 23:26:57 -0700

    update

--- js/custom.js
+++ js/custom.js
@@ -1,112 +1,92 @@
 let nav = document.getElementById("top-nav");
-let row1 = document.getElementById("skillsRow1");
-let row2 = document.getElementById("skillsRow2");
+let row1 = document.getElementById("skillsRow");
 let row1_activated = false;
-let row2_activated = false;
+
+let progressOptions = {
+    color: 'rgb(4, 83, 170)',
+    trailColor: 'rgba(4, 83, 170, 0.2)',
+    strokeWidth: 20,
+    duration: 1000,
+    easing: 'easeInOut',
+    text: {
+        value: 'Java',
+        style: {
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            padding: 0,
+            margin: 0,
+            fontSize: '1.75em',
+            transform: {
+                prefix: true,
+                value: 'translate(-50%, -50%)'
+            }
+        }
+    },
+    from: {color: 'rgb(4, 83, 170)'},
+    to: {color: 'rgb(4, 83, 170)'},
+    step: function (state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+    }
+};
 
 // PROGRESS SKILLS
 function activateRow1() {
-    let options = {
-        color: 'rgb(4, 83, 170)',
-        trailColor: 'rgba(4, 83, 170, 0.2)',
-        strokeWidth: 20,
-        duration: 1000,
-        easing: 'easeInOut',
-        text: {
-            value: 'Java',
-            style: {
-                position: 'absolute',
-                left: '50%',
-                top: '50%',
-                padding: 0,
-                margin: 0,
-                fontSize: '1.75em',
-                transform: {
-                    prefix: true,
-                    value: 'translate(-50%, -50%)'
-                }
-            }
-        },
-        from: {color: 'rgb(4, 83, 170)'},
-        to: {color: 'rgb(4, 83, 170)'},
-        step: function (state, circle) {
-            circle.path.setAttribute('stroke', state.color);
-        }
-    };
-    new ProgressBar.Circle('#progressJava', options).animate(0.9);
+    progressOptions.text.value = 'Java';
+    progressOptions.color = 'rgb(4, 83, 170)';
+    progressOptions.trailColor = 'rgba(4, 83, 170, 0.2)';
+    progressOptions.to.color = 'rgb(4, 83, 170)';
+    progressOptions.from.color = 'rgb(4, 83, 170)';
+    new ProgressBar.Circle('#progressJava', progressOptions).animate(0.9);
 
-    options.text.value = 'HTML';
-    options.color = 'rgb(239, 162, 50)';
-    options.trailColor = 'rgba(239, 162, 50, 0.2)';
-    options.to.color = 'rgb(239, 162, 50)';
-    options.from.color = 'rgb(239, 162, 50)';
-    new ProgressBar.Circle('#progressHTML', options).animate(0.9);
+    progressOptions.text.value = 'HTML';
+    progressOptions.color = 'rgb(239, 162, 50)';
+    progressOptions.trailColor = 'rgba(239, 162, 50, 0.2)';
+    progressOptions.to.color = 'rgb(239, 162, 50)';
+    progressOptions.from.color = 'rgb(239, 162, 50)';
+    new ProgressBar.Circle('#progressHTML', progressOptions).animate(0.9);
 
-    options.text.value = 'C++';
-    options.color = 'rgb(113, 128, 205)';
-    options.trailColor = 'rgba(113, 128, 205, 0.2)';
-    options.to.color = 'rgb(113, 128, 205)';
-    options.from.color = 'rgb(113, 128, 205)';
-    new ProgressBar.Circle('#progressCplusplus', options).animate(0.4);
+    progressOptions.text.value = 'C++';
+    progressOptions.color = 'rgb(113, 128, 205)';
+    progressOptions.trailColor = 'rgba(113, 128, 205, 0.2)';
+    progressOptions.to.color = 'rgb(113, 128, 205)';
+    progressOptions.from.color = 'rgb(113, 128, 205)';
+    new ProgressBar.Circle('#progressCplusplus', progressOptions).animate(0.4);
 
-    options.text.value = 'Python';
-    options.color = 'rgb(75, 152, 90)';
-    options.trailColor = 'rgba(75, 152, 90, 0.2)';
-    options.to.color = 'rgb(75, 152, 90)';
-    options.from.color = 'rgb(75, 152, 90)';
-    new ProgressBar.Circle('#progressPython', options).animate(0.2);
-}
+    progressOptions.text.value = 'Python';
+    progressOptions.color = 'rgb(75, 152, 90)';
+    progressOptions.trailColor = 'rgba(75, 152, 90, 0.2)';
+    progressOptions.to.color = 'rgb(75, 152, 90)';
+    progressOptions.from.color = 'rgb(75, 152, 90)';
+    new ProgressBar.Circle('#progressPython', progressOptions).animate(0.2);
 
-function activateRow2() {
-    let options = {
-        color: 'rgb(207, 92, 56)',
-        trailColor: 'rgba(207, 92, 56, 0.2)',
-        strokeWidth: 20,
-        duration: 1000,
-        easing: 'easeInOut',
-        text: {
-            value: 'JS',
-            style: {
-                position: 'absolute',
-                left: '50%',
-                top: '50%',
-                padding: 0,
-                margin: 0,
-                fontSize: '1.75em',
-                transform: {
-                    prefix: true,
-                    value: 'translate(-50%, -50%)'
-                }
-            }
-        },
-        from: {color: 'rgb(207, 92, 56)'},
-        to: {color: 'rgb(207, 92, 56)'},
-        step: function (state, circle) {
-            circle.path.setAttribute('stroke', state.color);
-        }
-    };
-    new ProgressBar.Circle('#progressJavascript', options).animate(0.85);
+    progressOptions.text.value = 'JS';
+    progressOptions.color = 'rgb(207, 92, 56)';
+    progressOptions.trailColor = 'rgba(207, 92, 56, 0.2)';
+    progressOptions.to.color = 'rgb(207, 92, 56)';
+    progressOptions.from.color = 'rgb(207, 92, 56)';
+    new ProgressBar.Circle('#progressJavascript', progressOptions).animate(0.85);
 
-    options.text.value = 'Scala';
-    options.color = 'rgb(164, 222, 44)';
-    options.trailColor = 'rgba(164, 222, 44, 0.2)';
-    options.to.color = 'rgb(164, 222, 44)';
-    options.from.color = 'rgb(164, 222, 44)';
-    new ProgressBar.Circle('#progressScala', options).animate(0.4);
+    progressOptions.text.value = 'Scala';
+    progressOptions.color = 'rgb(164, 222, 44)';
+    progressOptions.trailColor = 'rgba(164, 222, 44, 0.2)';
+    progressOptions.to.color = 'rgb(164, 222, 44)';
+    progressOptions.from.color = 'rgb(164, 222, 44)';
+    new ProgressBar.Circle('#progressScala', progressOptions).animate(0.4);
 
-    options.text.value = 'C';
-    options.color = 'rgb(92, 193, 176)';
-    options.trailColor = 'rgba(92, 193, 176, 0.2)';
-    options.to.color = 'rgb(92, 193, 176)';
-    options.from.color = 'rgb(92, 193, 176)';
-    new ProgressBar.Circle('#progressC', options).animate(0.5);
+    progressOptions.text.value = 'C';
+    progressOptions.color = 'rgb(92, 193, 176)';
+    progressOptions.trailColor = 'rgba(92, 193, 176, 0.2)';
+    progressOptions.to.color = 'rgb(92, 193, 176)';
+    progressOptions.from.color = 'rgb(92, 193, 176)';
+    new ProgressBar.Circle('#progressC', progressOptions).animate(0.5);
 
-    options.text.value = 'CSS';
-    options.color = 'rgb(136, 109, 155)';
-    options.trailColor = 'rgba(136, 109, 155, 0.2)';
-    options.to.color = 'rgb(136, 109, 155)';
-    options.from.color = 'rgb(136, 109, 155)';
-    new ProgressBar.Circle('#progressCSS', options).animate(0.85);
+    progressOptions.text.value = 'CSS';
+    progressOptions.color = 'rgb(136, 109, 155)';
+    progressOptions.trailColor = 'rgba(136, 109, 155, 0.2)';
+    progressOptions.to.color = 'rgb(136, 109, 155)';
+    progressOptions.from.color = 'rgb(136, 109, 155)';
+    new ProgressBar.Circle('#progressCSS', progressOptions).animate(0.85);
 }
 
 /*
@@ -128,13 +108,6 @@ window.onscroll = function () {
         if (scrollBot > row1.offsetTop && scrollTop < row1.offsetTop + row1.offsetHeight) {
             activateRow1();
             row1_activated = true;
-        }
-    }
-
-    if (!row2_activated) {
-        if (scrollBot > row2.offsetTop && scrollTop < row2.offsetTop + row2.offsetHeight) {
-            activateRow2();
-            row2_activated = true;
         }
     }
 };
